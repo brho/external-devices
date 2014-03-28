904d7fe0caaddefcecc329493864f5d669ff82c7
Barret Rhoden <brho@cs.berkeley.edu>
2014-03-28 14:16:17 -0700

    Plan9 relicense of drivers

--- etherigbe.c
+++ etherigbe.c
@@ -1,5 +1,10 @@
-/* This software is provided under the terms of the Lucent Public License,
- * Version 1.02. */
+/* This file is part of the UCB release of Plan 9. It is subject to the license
+ * terms in the LICENSE file found in the top-level directory of this
+ * distribution and at http://akaros.cs.berkeley.edu/files/Plan9License. No
+ * part of the UCB release of Plan 9, including this file, may be copied,
+ * modified, propagated, or distributed except according to the terms contained
+ * in the LICENSE file. */
+
 /*
  * Intel 8254[340]NN Gigabit Ethernet PCI Controllers
  * as found on the Intel PRO/1000 series of adapters:
--- ethermii.c
+++ ethermii.c
@@ -1,5 +1,10 @@
-/* This software is provided under the terms of the Lucent Public License,
- * Version 1.02. */
+/* This file is part of the UCB release of Plan 9. It is subject to the license
+ * terms in the LICENSE file found in the top-level directory of this
+ * distribution and at http://akaros.cs.berkeley.edu/files/Plan9License. No
+ * part of the UCB release of Plan 9, including this file, may be copied,
+ * modified, propagated, or distributed except according to the terms contained
+ * in the LICENSE file. */
+
 #include <vfs.h>
 #include <kfs.h>
 #include <slab.h>
